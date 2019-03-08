bd28e6c31404feca85d84801ee98d33fb404cc84
Vitaly Trushin <webtrushin@gmail.com>
2019-03-08 19:33:33 +0300

    js modified

--- begin/script.js
+++ begin/script.js
@@ -1,10 +1,17 @@
 'use strict';
 
+let money = prompt("Ваш бюджет на месяц?", 0);
+let time = prompt("Введите дату в формате YYYY-MM-DD",0);
+let dayBudget = money/30;
 
+let appData = {
+    budget:money,
+    timeData: time,
+    optionalExpenses: {},
+    income : {},
+    expenses: {},
+    savings : false
 
+};
 
-{
-   var c = 2;
-}
-
-console.log(confirm("d"));
+alert(dayBudget);
