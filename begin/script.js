d323d096eefe556d7ff44e00ea03fd3a72c4c022
Vitaly Trushin <webtrushin@gmail.com>
2019-03-09 20:50:18 +0300

    Budget modif

--- begin/script.js
+++ begin/script.js
@@ -1,8 +1,8 @@
 'use strict';
 
-let money = prompt("Ваш бюджет на месяц?", 0);
-let time = prompt("Введите дату в формате YYYY-MM-DD",0);
-let dayBudget = money/30;
+let money = +prompt("Ваш бюджет на месяц?", ""),
+    time = prompt("Введите дату в формате YYYY-MM-DD","");
+
 
 let appData = {
     budget:money,
@@ -14,4 +14,22 @@ let appData = {
 
 };
 
-alert(dayBudget);
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце",""),
+        b = prompt("Во сколько обойдется ?","");
+
+        if ( (typeof(a)) === 'string' && (typeof(a)) != null &&
+            (typeof(b)) != null && a != '' && b != '' && a.length < 50 ){
+        
+            appData.expenses[a] = b;
+        }
+    
+}
+
+appData.moneyPerDay = appData.budget / 30;
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100){ console.log("Минимальный уровень достатка");}
+else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){ console.log("Средний уровень достатка");}
+else if (appData.moneyPerDay > 2000){ console.log("Высокий уровень достатка");}
+else {console.log("Произошла ошибка.");}
